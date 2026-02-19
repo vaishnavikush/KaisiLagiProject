58a208b2484293699af85c0dc38ed6428c765d0e
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2026-02-19 18:37:30 +0530

    Update ReviewService.java

--- ReviewMaster/ReviewService.java
+++ ReviewMaster/ReviewService.java
@@ -1,8 +1,12 @@
 package com.example.kaisi_lagi.ReviewMaster;
 
+import com.example.kaisi_lagi.MovieMaster.MovieMaster;
 import com.example.kaisi_lagi.UserMaster.UserMaster;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Service
     public class ReviewService {
@@ -11,10 +15,19 @@ import org.springframework.stereotype.Service;
     ReviewRepository reviewRepository;
 
 
-        public void saveReview(ReviewMaster review, UserMaster user) {
+        public void saveReview(ReviewMaster review, UserMaster user, Model model, MovieMaster movie) {
 
             review.setUser(user);
             reviewRepository.save(review);
+
+            if (user != null) {
+                List<ReviewMaster> reviews = reviewRepository.findUserMovieReviewByObjects(user, movie);
+                if (!reviews.isEmpty()) {
+                    ReviewMaster r = reviews.get(0); // take the latest
+                    model.addAttribute("rateExists", r.getRating());
+                    model.addAttribute("commentExists", r.getComment());
+                }
+            }
         }
     }
 
